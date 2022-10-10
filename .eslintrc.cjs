d1f0bc87096d64baf9db60b65d9101835dfdc139
Ciffelia <mc.prince.0203@gmail.com>
2022-10-10 18:25:03 +0900

    refactor: remove workspaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['standard-with-typescript', 'eslint-config-prettier'],
   parserOptions: {
-    project: './packages/*/tsconfig.json',
+    project: './tsconfig.json',
   },
 };
